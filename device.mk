3110a38a5009ad666caa39e99fc403764adf25b8
Deivid21 <david.parra.ignacio@gmail.com>
2025-08-11 13:03:20 -0400

    fogo: Rebrand to penang
    Change-Id: Ia57273bf8e3622c5109dd3299b1d438d5f995dfb

--- device.mk
+++ device.mk
@@ -76,4 +76,4 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit from vendor blobs
-$(call inherit-product, vendor/motorola/fogo/fogo-vendor.mk)
+$(call inherit-product, vendor/motorola/penang/penang-vendor.mk)
